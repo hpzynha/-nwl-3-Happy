de6b5a2fef99b616e9d682ab1b15778267cee6ae
Larissa Nogueira da Rocha <larissa.nogueira.rocha@gmail.com>
2020-10-18 21:50:35 -0400

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const pages = require("./pages.js");
+
+const server = express();
+
+server
+// utilizar o body do req
+  .use(express.urlencoded({ extended: true }))
+  .use(express.static("public"))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "hbs")
+
+  .get("/", pages.index)
+  .get("/orphanage", pages.orphanage)
+  .get("/orphanages", pages.orphanages)
+  .get("/create-orphanage", pages.createOrphanage)
+  .post("/save-orphanage", pages.saveOrphanage)
+  .post('/save-orphanages', pages.saveOrphanage);
+
+server.listen(5500, () => {
+  console.log("Server started");
+});
